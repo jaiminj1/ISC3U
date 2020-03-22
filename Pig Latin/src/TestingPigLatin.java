37718bc5f29c9f7ce51ac010903e04a4938d30ef
Jaimin <jaiminjaffer123@gmail.com>
2020-03-21 22:30:41 -0400

    Delete TestingPigLatin.java

--- Pig Latin/src/TestingPigLatin.java
+++
@@ -1,9 +0,0 @@
-package piglatin;
-
-public class TestingPigLatin {
-
-    //public static void main(String args[]) {
-       // System.out.println(PigLatin.translateSentence("MSN"));
-   //     System.out.println(PigLatin.translateWord("MSN"));
-   // }
-}
